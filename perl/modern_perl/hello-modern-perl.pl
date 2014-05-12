6d5f4f6d15680965ecc4671a09fe88c9e0bbff43
David Jimenez-Morales <biodavidjm@gmail.com>
2014-05-12 16:43:35 -0500

    Modern::Perl works
    I have also started testing that change perl version also works very
    smoothly

--- perl/modern_perl/hello-modern-perl.pl
+++ perl/modern_perl/hello-modern-perl.pl
@@ -1,7 +1,17 @@
 #!/usr/bin/env perl
 
-use Modern::Perl '2013';
+use Modern::Perl;
 use autodie;
 
+# I need to install the last version of Perl
+say "Hello!";
 
-done_testing();
+  say "#$_" for 1 .. 10;
+
+for (1 .. 10)
+{
+    say "#$_";
+}
+
+my @squares = map { $_ * $_ } 1 .. 10;
+say for @squares;
