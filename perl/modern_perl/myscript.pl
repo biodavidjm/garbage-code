0e4df80212756ded4dcfd4a7a88bbdf23d5b5eac
David Jimenez-Morales <biodavidjm@gmail.com>
2014-05-22 22:46:40 -0500

    Playing with objects

--- perl/modern_perl/myscript.pl
+++ perl/modern_perl/myscript.pl
@@ -2,9 +2,10 @@
 
 use strict;
 
-# you may need to set @INC here (see below)
+# EXAMPLE 1. 
+# Helps understand when calling functions works and when it doesn't
 
-my @list = qw (O h ~ n o ~ w h a t ~ i s ~ t h i s !);
+# my @list = qw (O h ~ n o ~ w h a t ~ i s ~ t h i s !);
 
 # case 1
 # use MyModule;
@@ -22,6 +23,46 @@ my @list = qw (O h ~ n o ~ w h a t ~ i s ~ t h i s !);
 # print func2(@list),"\n";
 
 # case 4
-use MyModule qw(:Both);
-print func1(@list),"\n";
-print func2(@list),"\n";
+# use MyModule qw(:Both);
+# print func1(@list),"\n";
+# print func2(@list),"\n";
+
+# EXAMPLE 2. 
+# Helps understand funcions and closures.  In perl it is possible
+# to define dynamic subroutines, inside the code of the current scope. That
+# scope can be a function or even another dynamic subroutine. These subroutines,
+# which are sometimes referred to as closures, can see all the variables of the
+# scope in which they were declared, even after the function that created them
+# stopped running.
+
+use strict;
+use warnings;
+
+sub create_counter
+{
+    my $counter = 0;
+
+    my $counter_func = sub {
+
+        print $counter."  ";
+        return ($counter++);
+
+    };
+
+    return $counter_func;
+}
+
+my @counters = (create_counter(), create_counter());
+
+# Initialize the random number generator to a constant value;
+srand(24);
+
+for my $i (1 .. 100)
+{
+    # This call generates a random number that is either 0 or 1
+    my $which_counter = int(rand(2));
+
+    my $value = $counters[$which_counter]->();
+
+    print "$which_counter = $value\n";
+}
--- perl/modern_perl/objects.pl
+++ perl/modern_perl/objects.pl
@@ -1,3 +1,30 @@
-#!/usr/bin/env perl
+#!/usr/bin/perl
+
+# tutorials/perl-for-newbies/part3/
+
+use strict;
+use warnings;
+
+use Foo;
+use Bar;
+
+my $foo = Bar->new("MyFoo", 500);
+
+print $foo->get_name()." ";
+print $foo->get_number(). "\n";
+
+$foo->assign_name_ext("Shlomi Fish");
+
+$foo->assign_number(400);
+
+print $foo->get_name(), "\n";
+print $foo->get_number()."\n";
+
+print $foo->get_num_times_assigned()."\n";
+print $foo->get_num_times_assigned()."\n";
+
+
+
 
 # Chapter 7, page 110
+
