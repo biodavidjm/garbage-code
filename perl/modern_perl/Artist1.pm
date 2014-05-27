4208e531d7492d0be63ef10373e6b4cb7e194570
David Jimenez-Morales <biodavidjm@gmail.com>
2014-05-26 22:24:27 -0500

    get-set method express

--- perl/modern_perl/Artist1.pm
+++ perl/modern_perl/Artist1.pm
@@ -18,4 +18,28 @@ sub get_name {
 	return $self->{band}
 }
 
+# Get and Set method:
+
+sub album {
+	my $self = shift;
+	unless (ref $self) {
+		die "You should call album() with an object, not with a class\n";
+	}
+
+	# Receive more data
+	my $data = shift;
+	# Set the album
+	$self->{album} = $data if defined $data;
+
+	return $self->{album} 
+}
+
+# Or a minuature get-set method
+sub year {
+	$_[0]->{year}=$_[1] if defined $_[1]; $_[0]->{year}
+}
+sub style {
+	$_[0]->{style}=$_[1] if defined $_[1]; $_[0]->{style}	
+}
+
 1;
--- perl/modern_perl/test_artist.pl
+++ perl/modern_perl/test_artist.pl
@@ -4,13 +4,23 @@ use strict;
 use warnings;
 use Artist1;
 
-my $group = Artist->new();
+# Initialize the object without arguments
+# my $group = Artist->new();
 
 my $object = Artist->new (
 	band  => "Radiohead",
 	album => "OK Computer",
-	year  => "1997"
+	year  => "1997",
+	style => "Alternative Pop-Rock"
 );
 
 print "The band's name is ", $object->get_name, "\n";
+
+print "the band's title is ", $object->album(), "\n";
+$object->album("It might be wrong");
+print "the band's title is ", $object->album(), "\n";
+print "year = ", $object->year(),"\n";
+print "and the style ", $object->style(),"\n";
+$object->style("Alternative Electronic-Pop-Rock");
+print "and the style ", $object->style(),"\n";
 
