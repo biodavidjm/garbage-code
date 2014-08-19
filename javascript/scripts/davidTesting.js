f56f1666e87527a36a6408990b9a875c2cb51c95
David Jimenez-Morales <biodavidjm@gmail.com>
2014-08-19 16:45:30 -0500

    Adding up to level 2 of javascript road trip 2

--- javascript/scripts/davidTesting.js
+++ javascript/scripts/davidTesting.js
@@ -17,8 +17,39 @@ console.log("That's all\n");
 var parkIsOpen = true;
 
 if ( parkIsOpen) {
-  console.out("Welcome to the Badlands National Park! Try to enjoy your stay.");
+  console.log("Welcome to the Badlands National Park! Try to enjoy your stay.");
 }
 else{
-  console.out("Sorry, the Badlans are particularly bad today. We're closed!");
+  console.log("Sorry, the Badlans are particularly bad today. We're closed!");
 }
+
+console.log("The Dam Complex exercise");
+
+var totalGen = 19;
+var totalMW = 0;
+
+for (var generators = 1; generators <= totalGen; generators++ )
+{
+	var isEven = generators%2;
+	if (isEven == 0)
+	{
+		if (generators<=4)
+		{
+			totalMW = totalMW+62;
+			console.log("Generator #"+ generators +" is on, adding 62 MW, for a total of "+ totalMW +" MW!");
+		}
+		else
+		{
+			totalMW = totalMW+124;
+			console.log("Generator #"+ generators +" is on, adding 124 MW, for a total of "+ totalMW +" MW!");
+		}
+	}
+	else
+	{
+		console.log("Generator #" + generators + " is off.");
+	}
+}
+
+
+
+
