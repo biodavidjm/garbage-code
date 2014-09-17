2b0ef9d7636b8014ab76ec13b341baca20eb7497
David Jimenez-Morales <biodavidjm@gmail.com>
2014-09-17 11:56:07 -0500

    Javascript course finished!
    I have completed the javascript road trip part 3, a extremely long
    course!

--- javascript/scripts/testing-javascript.js
+++ javascript/scripts/testing-javascript.js
@@ -534,3 +534,483 @@ function dontPanic (location){
         "There be Pirates nearby! Stations!\n" + list);
 }
 dontPanic(lighthouseRock);
+
+// Object functionality
+// 1. I have to add this function...
+// function addRanger(location, name, skillz, station){
+//   location.numRangers++;
+//   location["ranger" + location.numRangers] = {
+//     name: name, 
+//     skillz: skillz, 
+//     station: station 
+//   }; 
+// }
+// to the object "lighthouseRock"
+
+var superBlinders = [ ["Firestorm", 4000], ["Solar Death Ray", 6000], ["Supernova", 12000] ];
+var lighthouseRock = {
+  gateClosed: true,
+  weaponBulbs: superBlinders,
+  capacity: 30,
+  secretPassageTo: "Underwater Outpost",
+  numRangers: 3,
+  ranger1: {name: "Nick Walsh", skillz: "magnification burn", station: 2},
+  ranger2: {name: "Drew Barontini", skillz: "uppercut launch", station: 3},
+  ranger3: {name: "Christine Wong", skillz: "bomb defusing", station: 1},
+  //Here it is
+  addRanger: function (name, skillz, station) 
+  {
+    this[name] = {skillz: skillz, station: station};
+  }
+};
+
+//Now let's add a new Ranger...
+lighthouseRock.addRanger("Jordan Wade", "dual-wield hand crossbow", 4);
+
+// Now,  I need to create a new function to add elements to the array of superBlinders
+var superBlinders = [ ["Firestorm", 4000], ["Solar Death Ray", 6000], ["Supernova", 12000] ];
+var lighthouseRock = {
+  gateClosed: true,
+  weaponBulbs: superBlinders,
+  capacity: 30,
+  secretPassageTo: "Underwater Outpost",
+  numRangers: 3,
+  ranger1: {name: "Nick Walsh", skillz: "magnification burn", station: 2},
+  ranger2: {name: "Drew Barontini", skillz: "uppercut launch", station: 3},
+  ranger3: {name: "Christine Wong", skillz: "bomb defusing", station: 1}, 
+  addRanger: function (name, skillz, station){
+    this.numRangers++;
+    this["ranger" + this.numRangers] = {
+      name: name, 
+      skillz: skillz, 
+      station: station 
+    }; 
+  },
+  addBulb: function(name, wattage){
+      var temp = [name, wattage];
+      lighthouseRock.weaponBulbs.push(temp);
+  }
+};
+
+//Now let's use the following function to add elements...
+lighthouseRock.addBulb = function (name, wattage){
+  this.weaponBulbs.push([name, wattage]);
+};
+// There we go:
+lighthouseRock.addBulb("Blasterbright", 5000);
+lighthouseRock.addBulb("Sight Slayer", 1800);
+lighthouseRock.addBulb("Burner of Souls", 7500);
+
+// A lot of text to describe this exercise:
+var vehicle3 = {
+  type: "Submarine", capacity: 8, storedAt: "Underwater Outpost",
+  ranger1: { name: "Gregg Pollack", skillz: "Lasering", dayOff: "Friday"},
+  ranger2: { name: "Bijan Boustani", skillz: "Roundhouse Kicks", dayOff: "Tuesday"},
+  ranger3: { name: "Ashley Smith", skillz: "Torpedoing", dayOff: "Friday"},
+  ranger4: { name: "Mark Krupinski", skillz: "Sniping", dayOff: "Wednesday"},
+  numRangers: 4
+};
+function relieveDuty (vehicle, day){
+  var offDuty = [ ];
+  var onDuty = [ ];
+  for(var i = 1; i<=vehicle["numRangers"]; i++){
+    if(vehicle["ranger" + i]["dayOff"] == day){
+      offDuty.push(vehicle["ranger" + i]);
+    }
+    else{
+      onDuty.push(vehicle["ranger" + i]);
+    }
+    delete vehicle["ranger" + i];
+  }
+  vehicle.numRangers -= offDuty.length;
+  for(var j = 1; j<=vehicle["numRangers"]; j++){
+    vehicle["ranger" + j] = onDuty.shift();
+  }
+  return offDuty;
+}
+var offToday = relieveDuty(vehicle3, "Friday");
+
+
+// Enumeration
+console.log("\nTime to include function to enumerate the properties and values of the objects within the objects");
+var rockSpearguns = {
+  Sharpshooter: {barbs: 2, weight: 10, heft: "overhand"},
+  Pokepistol: {barbs: 4, weight: 8, heft: "shoulder"},
+  Javelinjet: {barbs: 4, weight: 12, heft: "waist"},
+  Firefork: {barbs: 6, weight: 8, heft: "overhand"},
+  "The Impaler": {barbs: 1, weight: 30, heft: "chest"}
+};
+function listGuns (guns) {
+  for (var speargun in guns) {
+    console.log("Behold! "+speargun+", with "+guns[speargun].heft+" heft!");
+  }
+}
+listGuns(rockSpearguns);
+
+//Now, I have to include the function inside the object:
+console.log("\nNow the function is moving inside the object:");
+var rockSpearguns = {
+  Sharpshooter: {barbs: 2, weight: 10, heft: "overhand"},
+  Pokepistol: {barbs: 4, weight: 8, heft: "shoulder"},
+  Javelinjet: {barbs: 4, weight: 12, heft: "waist"},
+  Firefork: {barbs: 6, weight: 8, heft: "overhand"},
+  "The Impaler": {barbs: 1, weight: 30, heft: "chest"},
+  listGuns: function (){
+    for(var property in this){
+      if (this[property]["barbs"]){
+        console.log("Behold! " + property +
+                    ", with " + this[property]["heft"] +
+                    " heft!");      
+      }
+    }
+  }
+};
+//Use the fucking bracket notation to call the fucking function
+rockSpearguns['listGuns']();
+console.log("\nAlternatively, you can use the damn 'dot' notation:");
+rockSpearguns.listGuns();
+
+// LEVEL 5 of Javascript Roadtrip Part 3
+// Object prototype
+console.log("\nLEVEL 5 of JAVASCRIPT PART 3")
+
+console.log("- Create a method in an Array prototype:")
+var canyonCows = [
+  {name: "Bessie", type: "cow", hadCalf: "Burt"},
+  {name: "Donald", type: "bull", hadCalf: null},
+  {name: "Esther", type: "calf", hadCalf: null},
+  {name: "Burt", type: "calf", hadCalf: null},
+  {name: "Sarah", type: "cow", hadCalf: "Esther"},
+  {name: "Samson", type: "bull", hadCalf: null},
+  {name: "Delilah", type: "cow", hadCalf: null}
+];
+
+Array.prototype.countCattle = function(animal){
+  var numberAnimal = 0;
+  for (var i = 0;i<this.length;i++){
+    if (this[i].type == animal){
+      numberAnimal++;
+    }
+  }
+  return numberAnimal;
+}
+
+var tellme = canyonCows.countCattle("cow");
+console.log(tellme);
+
+//Next exercise
+console.log("\nCount the number of elements adding function to the Array prototype...")
+var canyonCows = [
+  {name: "Bessie", type: "cow", hadCalf: "Burt"},
+  {name: "Donald", type: "bull", hadCalf: null},
+  {name: "Esther", type: "calf", hadCalf: null},
+  {name: "Burt", type: "calf", hadCalf: null},
+  {name: "Sarah", type: "cow", hadCalf: "Esther"},
+  {name: "Samson", type: "bull", hadCalf: null},
+  {name: "Delilah", type: "cow", hadCalf: null}
+];
+var valleyCows = [
+  {name: "Danielle", type: "cow", hadCalf: null},
+  {name: "Brittany", type: "cow", hadCalf: "Christina"},
+  {name: "Jordan", type: "bull", hadCalf: null},
+  {name: "Trevor", type: "bull", hadCalf: null},
+  {name: "Christina", type: "calf", hadCalf: null},
+  {name: "Lucas", type: "bull", hadCalf: null}
+];
+var forestCows = [
+  {name: "Legolas", type: "calf", hadCalf: null},
+  {name: "Gimli", type: "bull", hadCalf: null},
+  {name: "Arwen", type: "cow", hadCalf: null},
+  {name: "Galadriel", type: "cow", hadCalf: null},
+  {name: "Eowyn", type: "cow", hadCalf: "Legolas"}
+];
+
+Array.prototype.countCattle = function ( kind ){
+  var numKind = 0;
+  for(var i = 0; i<this.length; i++){
+    if(this[i].type == kind){
+      numKind++;
+    }
+  }
+  return numKind;
+};
+console.log(canyonCows.countCattle("calf")+valleyCows.countCattle("bull")+forestCows.countCattle("cow"));
+
+// Hijos de puta!! esta la tenía bien!!!!
+console.log("\nNow we need two functions here added to the object and array prototype");
+
+var forestCows = [
+  {name: "Legolas", type: "calf", hadCalf: null},
+  {name: "Gimli", type: "bull", hadCalf: null},
+  {name: "Arwen", type: "cow", hadCalf: null},
+  {name: "Galadriel", type: "cow", hadCalf: null},
+  {name: "Eowyn", type: "cow", hadCalf: "Legolas"}
+];
+
+Object.prototype.noCalvesYet = function() {
+  if (this.type == "cow" && this.hadCalf == null) {
+      return true;
+    }
+    return false;
+};
+
+Array.prototype.countForBreeding = function() {
+  var numToBreed = 0;
+  for (var i = 0; i<this.length;i++){
+    if (this[i].noCalvesYet()){
+      numToBreed++;
+    }
+  }
+  return numToBreed;
+};
+
+console.log(forestCows.countForBreeding());
+
+console.log("\nAnd now I have to use this functions to go through this list and provide the number:");
+
+var canyonCows = [
+  {name: "Bessie", type: "cow", hadCalf: "Burt"},
+  {name: "Donald", type: "bull", hadCalf: null},
+  {name: "Esther", type: "calf", hadCalf: null},
+  {name: "Burt", type: "calf", hadCalf: null},
+  {name: "Sarah", type: "cow", hadCalf: "Esther"},
+  {name: "Samson", type: "bull", hadCalf: null},
+  {name: "Delilah", type: "cow", hadCalf: null}
+];
+
+var valleyCows = [
+  {name: "Danielle", type: "cow", hadCalf: null},
+  {name: "Brittany", type: "cow", hadCalf: "Christina"},
+  {name: "Jordan", type: "bull", hadCalf: null},
+  {name: "Trevor", type: "bull", hadCalf: null},
+  {name: "Christina", type: "calf", hadCalf: null},
+  {name: "Lucas", type: "bull", hadCalf: null}
+];
+
+var forestCows = [
+  {name: "Legolas", type: "calf", hadCalf: null},
+  {name: "Gimli", type: "bull", hadCalf: null},
+  {name: "Arwen", type: "cow", hadCalf: null},
+  {name: "Galadriel", type: "cow", hadCalf: null},
+  {name: "Eowyn", type: "cow", hadCalf: "Legolas"}
+];
+
+var badlandsCows = [
+  {name: "Voldemort", type: "bull", hadCalf: null},
+  {name: "Maleficent", type: "cow", hadCalf: null},
+  {name: "Ursula", type: "cow", hadCalf: "Draco"},
+  {name: "Draco", type: "calf", hadCalf: null},
+  {name: "Joker", type: "bull", hadCalf: null},
+  {name: "Chucky", type: "calf", hadCalf: null},
+  {name: "Samara", type: "cow", hadCalf: "Chucky"}
+];
+console.log("What a minute. What's going on with "+forestCows.countForBreeding());
+var numPriorityCows = canyonCows.countForBreeding()+valleyCows.countForBreeding()+forestCows.countForBreeding()+badlandsCows.countForBreeding();
+console.log("\tHerd-merger has indicated "+numPriorityCows+" cows of top breeding priority.");
+
+//INHERETANCE AND CONSTRUCTOR
+console.log("\nTime for Inheritance:");
+var genericPost = {
+  x: 0, 
+  y: 0, 
+  postNum: undefined,
+  connectionsTo: undefined,
+  sendRopeTo: function ( connectedPost ) {
+     if(this.connectionsTo == undefined){
+       var postArray = [ ];
+       postArray.push(connectedPost);
+       this.connectionsTo = postArray;
+     } else {
+       this.connectionsTo.push(connectedPost);
+     }
+   }
+  };
+
+console.log(genericPost);
+
+var post1 = Object.create(genericPost);
+var post2 = Object.create(genericPost);
+post1.x = -2;
+post1.y = 4;
+post1.postNum = 1;
+
+post2.x = 5;
+post2.y = 1;
+post2.postNum = 2;
+
+post1.connectionsTo = post2;
+post2.connectionsTo = post1;
+
+
+console.log(post1);
+console.log(post2);
+
+// Next challenge:
+var post8 = Object.create(genericPost);
+var post9 = Object.create(genericPost);
+var post10 = Object.create(genericPost);
+
+post8.x = 0;
+post8.y = -3;
+post8.postNum = 8;
+post8.sendRopeTo(post10);
+post9.x = 6;
+post9.y = 8;
+post9.postNum = 9;
+post9.sendRopeTo(post10);
+
+post10.x = -2;
+post10.y = 3;
+post10.postNum = 10;
+post10.sendRopeTo(post8);
+post10.sendRopeTo(post9);
+post9.numBirds = 0;
+post10.weathervane = "N";
+post8.lightsOn = false;
+post10.lightsOn = false;
+
+//Building a constructor
+// I got it fucking right!!
+// function Fencepost (xX, yY, postNUMB) {
+//   this.x = xX;
+//   this.y = yY;
+//   this.postNum = postNUMB;
+//   this.connectionsTo = [];
+//   this.sendRopeTo = function(connectedPost) {
+//        this.connectionsTo.push(connectedPost);
+//   };
+// }
+//This was the solution!!
+function Fencepost (x, y, postNum){
+  this.x = x;
+  this.y = y;
+  this.postNum = postNum;
+  this.connectionsTo = [];
+  this.sendRopeTo = function ( connectedPost ){
+    this.connectionsTo.push(connectedPost);
+  };
+}
+
+var post18 = new Fencepost(-3, 4, 18);
+var post19 = new Fencepost(5,-1,19);
+var post20 = new Fencepost(-2,10,20);
+
+// post18.sendRopeTo(post20);
+// post20.sendRopeTo(post18);
+// post18.sendRopeTo(post19);
+// post19.sendRopeTo(post18);
+
+//Now Identify the portions of the constructor that should be available to everybody
+// function Fencepost (x, y, postNum){
+//   this.x = x;
+//   this.y = y;
+//   this.postNum = postNum;
+//   this.connectionsTo = [];
+// }
+
+// function Fencepost(x, y, postNum){
+//   this.x = x;
+//   this.y = y;
+//   this.postNum = postNum;
+//   this.connectionsTo = [];
+// }
+
+// Fencepost.prototype = 
+// {
+//   sendRopeTo: function ( connectedPost )
+//   {
+//     this.connectionsTo.push(connectedPost);
+//   },
+//   removeRope: function ( removeTo )
+//   {
+//     var temp = [];
+//     for(var i = 0; i<this.connectionsTo.length; i++)
+//     {
+//        if(this.connectionsTo[i].postNum != removeTo)
+//        {
+//          temp.push(this.connectionsTo[i]);
+//        }
+//     }
+//   },
+//   movePost: function (x, y)
+//   {
+//     this.x = x;
+//     this.y = y;
+//   },
+// }
+
+// OVERRIDING PROTOTYPAL METHODS
+var x = 4;
+var y = "4";
+console.log(x.valueOf()+" and y = "+y.valueOf());
+if (x.valueOf()===y.valueOf())
+{
+  console.log("Yep, they are equal");
+}
+else
+{
+  console.log("noooooop, they are NOT equal");
+}
+
+console.log("\nNow let's override the native valueOf");
+
+Fencepost.prototype.valueOf = function () {
+  var pre = ( Math.pow(this.x, 2) + Math.pow(this.y,2) );
+  var straightLineDistance =  Math.sqrt(pre);
+  return straightLineDistance;
+};
+
+var xx = Math.pow(x,2);
+console.log(xx);
+
+console.log(post18.valueOf());
+
+console.log("\n\nAND FINALLY, finally, the final exercise, override the toString method...");
+
+var post18 = new Fencepost(-3, 4, 18);
+var post19 = new Fencepost(5,-1,19);
+var post20 = new Fencepost(-2,10,20);
+
+function Fencepost (x, y, postNum){
+  this.x = x;
+  this.y = y;
+  this.postNum = postNum;
+  this.connectionsTo = [];
+}
+
+Fencepost.prototype = {
+  sendRopeTo: function ( connectedPost ){
+    this.connectionsTo.push(connectedPost);
+  },
+  removeRope: function ( removeTo ){
+    var temp = [];
+    for(var i = 0; i<this.connectionsTo.length; i++){
+      if(this.connectionsTo[i].postNum != removeTo){
+        temp.push(this.connectionsTo[i]);
+      }
+    }
+    this.connectionsTo = temp;
+  },
+  movePost: function (x, y){
+    this.x = x;
+    this.y = y;
+  },
+  valueOf: function (){
+  return Math.sqrt( this.x*this.x + this.y*this.y );
+  }
+};
+
+Fencepost.prototype.toString = function() {
+  var list = "";
+  for (var i = 0; i < this.connectionsTo; i++)
+  {
+    list = list + this.connectionsTo[i].postNum + "\n";
+  }
+  return "Fence post #"+this.postNum+":\n"+
+  "Connected to posts:\n" + list +
+  "Distance from ranch:"+this.valueOf()+" yards\n";
+}
+
+post18.toString();
+
+
