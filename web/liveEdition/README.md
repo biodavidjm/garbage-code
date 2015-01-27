fe38011842ebbcd709bb9ff28f4f7df388d5aa11
David Jimenez-Morales <biodavidjm@gmail.com>
2015-01-27 16:35:10 -0600

    Live Edition using angular-xeditable
    These are the minimal code for live edition using `angular-xeditable`
    in a `yeoman angular` project. For more information, check the
    `README.md` of the folder.

---
+++ web/liveEdition/README.md
@@ -0,0 +1,13 @@
+README
+===
+
+## Installations
+
+Follow instructions [on the main page](http://vitalets.github.io/angular-xeditable/)
+
+* Install `bower install angular-xeditable `
+
+* Include in `index.html`
+
+<link href="bower_components/angular-xeditable/dist/css/xeditable.css" rel="stylesheet">
+<script src="bower_components/angular-xeditable/dist/js/xeditable.js"></script>
--- web/liveEdition/app/scripts/controllers/main.js
+++ web/liveEdition/app/scripts/controllers/main.js
@@ -7,11 +7,17 @@
  * # MainCtrl
  * Controller of the liveEditionApp
  */
+
 angular.module('liveEditionApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+	.controller('MainCtrl', function ($scope) {
+		$scope.awesomeThings = [
+		  'HTML5 Boilerplate',
+		  'AngularJS',
+		  'Karma'
+		];
+		$scope.here = {
+			isTheHead: 'Dictyostelium discoideum',
+			isTheTitle: 'Testing Live Edition',
+			desc: 'What would you like to say about dicty?\nAll right, write it down here'
+		};
+	});
