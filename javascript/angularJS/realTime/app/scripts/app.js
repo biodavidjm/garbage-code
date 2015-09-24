a5b5f1ecc9ec3c5819c1847778f346bbad4c2d59
David Jimenez-Morales <biodavidjm@gmail.com>
2015-09-24 17:40:50 -0500

    Showing the user profile
    `Synchronous XMLHttpRequest on the main thread is deprecated` which
    prevent the app to show all the comments by an user. But this will go
    away once I update the packages…

--- javascript/angularJS/realTime/app/scripts/app.js
+++ javascript/angularJS/realTime/app/scripts/app.js
@@ -49,6 +49,10 @@ angular
           }
         }
       })
+      .when('/users/:userId', {
+        templateUrl: 'views/profile.html',
+        controller: 'ProfileCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
